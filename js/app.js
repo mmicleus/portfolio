3f04f6da3b0f8c8f7adca818229e26a90350500b
Marik Micleusanu <yourEmail@example.com>
2023-08-29 23:50:07 +0100

    ok

--- js/app.js
+++ js/app.js
@@ -87,7 +87,8 @@ let projects = [
         modal:{
             title:"Bike rental website",
             desc: "This website allows users to book one or more bikes and choose from several models as well as the quantity, date and time. It uses Firebase for the backend.The website was built using Angular, namely Angular Router, Forms, Authentication, and HttpClient.",
-            src:["https://github.com/mmicleus/portfolio/blob/main/img/biking/Biking.ie.png?raw=true"],
+            // src:["https://github.com/mmicleus/portfolio/blob/main/img/biking/Biking.ie.png?raw=true"],
+            src:["https://github.com/mmicleus/portfolio/blob/main/img/airport/airport.png?raw=true","https://github.com/mmicleus/portfolio/blob/main/img/airport/airport2.png?raw=true","https://github.com/mmicleus/portfolio/blob/main/img/airport/airport3.png?raw=true","https://github.com/mmicleus/portfolio/blob/main/img/airport/airport4.png?raw=true"],
             github:"https://github.com/mmicleus/Bike-rental",
             demo:"https://mmicleus.github.io/Bike-rental/",
             technologies:["devicons devicons-html5","devicons devicons-css3","devicons devicons-bootstrap","devicons devicons-javascript","devicons devicons-angular","devicons devicons-firebase"]
